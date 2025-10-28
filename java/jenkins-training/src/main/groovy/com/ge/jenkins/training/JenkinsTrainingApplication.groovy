5e2be1681ed6ef9e9f9e1b3cae6e7be20b2c07fd
Bogdan Solga <bogdan.solga@gmail.com>
2025-10-28 11:23:08 +0200

    [dev] Initial commit of the two projects

---
+++ java/jenkins-training/src/main/groovy/com/ge/jenkins/training/JenkinsTrainingApplication.groovy
@@ -0,0 +1,13 @@
+package com.ge.jenkins.training
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JenkinsTrainingApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(JenkinsTrainingApplication, args)
+	}
+
+}
