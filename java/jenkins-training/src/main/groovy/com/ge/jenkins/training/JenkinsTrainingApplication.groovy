755aaba1ab808418f664dd2794614a44dde1823f
Bogdan Solga <bogdan.solga@gmail.com>
2025-10-30 14:25:58 +0200

    [clean] Removed Spring from the repo

--- java/jenkins-training/src/main/groovy/com/ge/jenkins/training/JenkinsTrainingApplication.groovy
+++ java/jenkins-training/src/main/groovy/com/ge/jenkins/training/JenkinsTrainingApplication.groovy
@@ -1,13 +1,8 @@
 package com.ge.jenkins.training
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication
 class JenkinsTrainingApplication {
 
 	static void main(String[] args) {
-		SpringApplication.run(JenkinsTrainingApplication, args)
 	}
 
 }
